f28a2b6a247dc4a9eeba01763f268182aa0868fb
2030428 <72548933+2030428@users.noreply.github.com>
2020-12-10 13:45:11 +0000

    script added for variable reading - added badfollower

---
+++ Assets/Scripts/FollowerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+
+
+public class FollowerController : MonoBehaviour
+{
+    public int goodFollowers, badfollowers, neutralFollowers;
+    public  InMemoryVariableStorage yarnStorage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (yarnStorage.GetValue("$badfollowers") != null)
+        {
+            yarnStorage.SetValue("$badfollowers", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        badfollowers = (int)yarnStorage.GetValue("$badfollowers").AsNumber;
+        Debug.Log("goodFollowers" + goodFollowers + "badfollowers" + badfollowers + "neutralFollowers" + neutralFollowers);
+
+    }
+}
